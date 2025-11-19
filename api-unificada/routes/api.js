81283fcc9341d57aded832b543606a5e8fec69b1
mariinaaaa <123450213+Mariinaaa@users.noreply.github.com>
2025-11-19 17:59:49 +0100

    Eliminada carpeta api-unificada
    Eliminada carpeta api-unificada de la rama services para añadirla a la de la rama del tema 3 (services_t3)

--- api-unificada/routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/integrationController');
-
-router.get('/clientes/detalle', ctrl.getClientesDetalle);
-router.get('/resumen', ctrl.getResumen);
-
-module.exports = router;
